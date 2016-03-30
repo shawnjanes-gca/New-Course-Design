041c8035d8c188f331ea30e0135818a7e4edfdf6
danielledenton-gca <danielledenton@greeneconsults.com>
2016-03-30 13:28:21 -0400

    Added new widget
    Added new widget Review Exercise Layer with Button and updated the
    insert bar

---
+++ _plug-ins/Greene Toolbar (Dreamweaver)-v2/Greene Custom 2/Review Exercise Layer with Button.js
@@ -0,0 +1,33 @@
+// Copyright 1999 Macromedia, Inc. All rights reserved.
+
+//---------------   GLOBAL VARIABLES   ---------------
+
+var helpDoc = MM.HELP_objMeta;
+layerno=1;
+
+//---------------     API FUNCTIONS    ---------------
+
+function objectTag() {
+ var Form = document.LayerForm;
+  var retVal='\n'+
+  '<div class="rvLayerOuterDiv">\n'+
+	  '<div id="Layerwbutton'+layerno+'" class="rvLayerInnerDiv">\n'+ 
+		'<table id="rvLayerButton">\n'+
+		  '<tr>\n'+ 
+			'<td>\n'+
+			  '<p>Correct.</p>\n'+
+			  '<p align="center">\n'+
+				'<input type="submit" name="Submit" value="CLOSE" class="closeLayer-btn" onClick="MM_showHideLayers(\'Layerwbutton'+layerno+'\',\'\',\'hide\')">\n'+
+			  '</p>\n'+
+			'</td>\n'+
+		  '</tr>\n'+
+		'</table>\n'+
+	'</div>\n';
+  layerno=layerno+1;
+ 
+  return retVal;
+}
+
+//---------------    LOCAL FUNCTIONS   ---------------
+
+
